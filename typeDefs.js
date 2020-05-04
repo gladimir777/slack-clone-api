0c328dbd2ba795cb77f111e69a80eaae534896ec
gladimir.jacobsen <gladimir.jacobsen@bovinoapp.com>
2020-05-03 23:31:29 -0400

    Create tables and connect to the database

--- typeDefs.js
+++ typeDefs.js
@@ -1,4 +1,31 @@
 export const typeDefs = `
+type Team{
+  owner:User!,
+  members:[User!]!
+  channels:[Channel!]!,
+  name:String!
+}
+
+type User{
+  id:Int!,
+  username:String!,
+  email:String!,
+  teams:[Team!]!
+}
+
+type Channel{
+  id:Int!,
+  name:String!,
+  users:[User!]!,
+  public:Boolean
+}
+
+type Mesage{
+  id:Int!,
+  text:String!,
+  user:User!,
+  Channel:Channel!
+}
   type Query { books: [Book] }
   type Book { title: String, author: String }
 `;
